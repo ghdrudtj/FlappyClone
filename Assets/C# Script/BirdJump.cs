5e5b7b98e2abbbb76c5306382fff27f2afd196c6
ghdrudtj <163072081+ghdrudtj@users.noreply.github.com>
2024-03-19 12:41:47 +0900

    완성

--- Assets/C# Script/BirdJump.cs
+++ Assets/C# Script/BirdJump.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Android;
+using UnityEngine.SceneManagement;
 
 public class BirdJump : MonoBehaviour
 {
     Rigidbody2D rb;
+    AudioSource audioSource;
 
     public float JumpPower;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -19,8 +22,17 @@ public class BirdJump : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * JumpPower;
+            audioSource.Play();
         }
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Score.score > Score.bestscore)
+        {
+            Score.bestscore = Score.score;
+        }
+        SceneManager.LoadScene("GameOverScene");
     }
 }
         
